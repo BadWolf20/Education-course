84c7381c114ecfcde7594181236c21aa1bfa1ca6
BadWolf20 <Roman.Pichugin20@gmail.com>
2021-11-15 16:04:15 +0700

    Add label, button, textfield
    Without constraints

--- Education course/ViewController.swift
+++ Education course/ViewController.swift
@@ -3,13 +3,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //MARK:  - Outlets
+    @IBOutlet var labelResult: UILabel!
+    @IBOutlet var texfieldName: UITextField!
+    @IBOutlet var buttonPushSettings: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
     }
 
     func setupView(){
-        view.backgroundColor = .systemRed
+        view.backgroundColor = .systemBlue
+        buttonPushSettings.backgroundColor = .systemIndigo
+    }
+
+    @IBAction func buttonPush(_ sender: Any) {
+        if !(texfieldName.text?.isEmpty ?? true){
+            labelResult.text = texfieldName.text
+            texfieldName.text = ""
+        }
     }
 }
 
